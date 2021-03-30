8e222def347533181b1583ce005689ff05d8f1f8
Pedro Honda <pedro.koh@gmail.com>
2021-03-29 21:34:24 -0300

    2021-03-29 : cleaning up rest_api

--- investing_service/static/js/rest_api.js
+++ investing_service/static/js/rest_api.js
@@ -1,24 +1,3 @@
-function httpGET(url, callback) {
-    const http = new XMLHttpRequest();
-    http.open("GET", url, true);
-    http.onreadystatechange=(e)=>{
-        if (http.readyState == 4 && http.status == 200)
-            callback(http.responseText);
-    }
-    http.send();
-}
-
-function httpPOST(url, callback, json) {
-    const http = new XMLHttpRequest();
-    http.open("POST", url, true);
-    http.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
-    http.onreadystatechange=(e)=>{
-        if (http.readyState == 4)
-            callback(http.responseText, http.status);
-    }
-    http.send(JSON.stringify(json));
-}
-
 function postValues() {
     var inputDateVar = document.getElementById("date").value;
     var inputStock = document.getElementById("stock").value;
@@ -50,29 +29,8 @@ function postValues() {
         contentType: 'application/json',
         data: JSON.stringify(json),
         dataType: 'json',
-        success: alert("SUCCESS!")
+        success: function(result) {
+            location.replace("http://127.0.0.1:8000/view/stock/" + inputStock);
+        }
     });
-    //httpPOST("http://127.0.0.1:8080/stock", callbackPOST, json);
-}
-
-function callbackPOST (response, status) {
-    if (status == 200) {
-        document.getElementById("date").value = "";
-        document.getElementById("stock").value = "";
-        document.getElementById("quantity").value = "";
-        document.getElementById("value").value = "";
-        document.getElementById("taxes").value = "";
-        document.getElementById("broker").value = "Broker";
-        document.getElementById("classes").value = "Classes";
-        document.getElementById("comments").value = "Comments...";
-        //httpGET("http://127.0.0.1:8080/stock", insertLast);
-    } else if (status == 409) {
-        setTimeout(function(){
-            alert(response);
-        },2000);
-    } else {
-        setTimeout(function(){
-            alert("Shenanigans!!");
-        },2000);
-    }
 }
